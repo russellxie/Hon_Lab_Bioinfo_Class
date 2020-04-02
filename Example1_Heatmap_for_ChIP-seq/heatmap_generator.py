03c02868b532101e3b978e4d73caaed9617ce775
Russellxie <russellxie@gmail.com>
2020-04-02 16:03:37 -0500

    Update the main script.

--- Example1_Heatmap_for_ChIP-seq/heatmap_generator.py
+++ Example1_Heatmap_for_ChIP-seq/heatmap_generator.py
@@ -1,37 +1,54 @@
 #!/usr/bin/env python3
+
+import os
 import sys
+import os.path
 import subprocess
 import argparse
 
+from os import path
 import pandas as pd
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--fq1', help='fastq1 - mRNA reads')
-args = parser.parse_args()
+def bin_generator(bed_file, sample_name, bin_num: int, bin_size: int):
+    #generate the bin file name
+    bin_file = "Region_bins_%s_%d_%d.saf"%(sample_name, bin_num, bin_size)
 
-def bin_generator():
-    for line in sys.stdin:
-        current_chr = line.strip().split('\t')[0]
-        left = int(line.strip().split('\t')[1])
-        right = int(line.strip().split('\t')[2])
+    #check if the output file exists
+    if path.exists(bin_file):
+        os.remove(bin_file)
 
-        left_point = round((left + right)/2) - 2000
+    outfh = open(bin_file, 'w')
+    with open(bed_file) as bedfh:
+        for line in bedfh:
+            current_chr = line.strip().split('\t')[0]
+            left = int(line.strip().split('\t')[1])
+            right = int(line.strip().split('\t')[2])
 
-        region_id = current_chr + '_' + str(left) + '_' + str(right)
+            if bin_num % 2 == 0:
+                left_point = int((left + right)/2) - (bin_num * bin_size) / 2
+            else:
+                left_point = left - int(bin_num / 2) * bin_size
+
+            region_id = current_chr + '_' + str(left) + '_' + str(right)
         
-        for i in range(0, 80):
+        for i in range(0, bin_num):
             print(region_id + '-' + str(i), end = '\t')
-            bin_left = left_point + (i*50)
-            bin_right = left_point + (i+1)*50 - 1
+            bin_left = left_point + (i*bin_size)
+            bin_right = left_point + (i+1)*bin_size - 1
 
             bin_list = [current_chr, str(bin_left), str(bin_right)]
             
-            print('\t'.join(bin_list) + '\t' + '+')
+            print('\t'.join(bin_list) + '\t' + '+', file = outfh)
+    outfh.close()
 
-def mat_generator():
-    total_reads = int(sys.argv[1])
+def mat_generator(marker):
 
+    #get the total reads from the featureCounts output file
+    summary_df = pd.read_csv("enhancer_bins.hg38.%s.counts.summary"%(marker), skiprows=1, index_col=0, header=False)
+    total_reads = np.sum(summary_df.values)
+
+    #read the output of featureCounts and convert it to a matrix
     line1 = sys.stdin.readline().strip()
     region_id = line1.strip().split('\t')[0].split('-')[0]
     reads = list(map(int, line1.strip().split('\t')[6:]))
@@ -59,14 +76,24 @@ def mat_generator():
 def plot_heatmap():
     ###
     # Generate the bins
+    bin_generator(args.region, args.sample_name, args.bin_num, args.bin_size)
+
     # Count the reads within each bin by using featureCounts
     command = """featureCounts -a {0}\
                 -F SAF\
                 -o enhancer_bins.hg38.{1}.counts\
                 -O\
-                {2}""".format(saf_file, marker, bam_file)
+                {2}""".format(saf_file, args.marker, args.bam)
 
     rc = subprocess.call(command, shell=True)
 
     # Reshape the results, calculate the CPM and output the matrix
     ###
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--region', help='region info in bed format.')
+parser.add_argument('--sample_name', help='sample_name for the file.')
+parser.add_argument('--bam', help='bam file input.')
+parser.add_argument('--bin_num', help='Bin numbers')
+parser.add_argument('--bin_size', help='Bin size')
+args = parser.parse_args()

e5a663e6723887ddc4353368e4b5e40f69b933ef
Russellxie <russellxie@gmail.com>
2020-04-02 17:43:11 -0500

    Update the code

--- Example1_Heatmap_for_ChIP-seq/heatmap_generator.py
+++ Example1_Heatmap_for_ChIP-seq/heatmap_generator.py
@@ -10,9 +10,7 @@ from os import path
 import pandas as pd
 import numpy as np
 
-def bin_generator(bed_file, sample_name, bin_num: int, bin_size: int):
-    #generate the bin file name
-    bin_file = "Region_bins_%s_%d_%d.saf"%(sample_name, bin_num, bin_size)
+def bin_generator(bed_file, bin_file, bin_num: int, bin_size: int):
 
     #check if the output file exists
     if path.exists(bin_file):
@@ -42,10 +40,14 @@ def bin_generator(bed_file, sample_name, bin_num: int, bin_size: int):
             print('\t'.join(bin_list) + '\t' + '+', file = outfh)
     outfh.close()
 
-def mat_generator(marker):
+def mat_converter(sample_name, count_file, matrix_file):
 
     #get the total reads from the featureCounts output file
-    summary_df = pd.read_csv("enhancer_bins.hg38.%s.counts.summary"%(marker), skiprows=1, index_col=0, header=False)
+    summary_df = pd.read_csv(f"enhancer_bins.hg38.{sample_name}.counts.summary",\
+                            skiprows=1,\
+                            index_col=0,\
+                            header=False)
+
     total_reads = np.sum(summary_df.values)
 
     #read the output of featureCounts and convert it to a matrix
@@ -56,39 +58,47 @@ def mat_generator(marker):
 
     print(region_id + '\t' + str(cpm), end = '')
 
-    for line in sys.stdin:
-        current_region_id = line.strip().split('\t')[0].split('-')[0]
-        chr_name = line.strip().split('\t')[1]
-        reads = list(map(int, line.strip().split('\t')[6:]))
+    outfh = open(matrix_file, 'w')
 
-        cpm = np.sum(reads) / total_reads * 1e6
-        
-        if current_region_id == region_id:
-            print ('\t', end ='')
-            print (cpm, end = '')
-        else:
-            print ('\n', end ='')
-            print (current_region_id + '\t' + str(cpm), end = '')
-            region_id = current_region_id
+    with open(count_file) as cf:
+        for line in cf:
+            current_region_id = line.strip().split('\t')[0].split('-')[0]
+            _chr_name = line.strip().split('\t')[1]
+            reads = list(map(int, line.strip().split('\t')[6]))
 
-    print('\n', end='')
+            cpm = np.sum(reads) / total_reads * 1e6
+            
+            if current_region_id == region_id:
+                print ('\t', end ='', file=outfh)
+                print (cpm, end = '', file=outfh)
+            else:
+                print ('\n', end ='', file=outfh)
+                print (current_region_id + '\t' + str(cpm), end = '', file=outfh)
+                region_id = current_region_id
 
-def plot_heatmap():
-    ###
+    print('\n', end='', file=outfh)
+    outfh.close()
+
+def plot_heatmap(sample_name, bam_file, bin_num, bin_size):
+    #define the output file names
+    bin_file = f"Region_bins.{sample_name}.{str(bin_num)}X{str(bin_size)}.saf"
+    count_file = f"Region_bins.{sample_name}.{str(bin_num)}X{str(bin_size)}.counts"
+    matrix_file = f"Region_bins.{sample_name}.{str(bin_num)}X{str(bin_size)}.matrix.csv"
+
     # Generate the bins
-    bin_generator(args.region, args.sample_name, args.bin_num, args.bin_size)
+    bin_generator(args.region, bin_file, args.bin_num, args.bin_size)
 
     # Count the reads within each bin by using featureCounts
-    command = """featureCounts -a {0}\
-                -F SAF\
-                -o enhancer_bins.hg38.{1}.counts\
-                -O\
-                {2}""".format(saf_file, args.marker, args.bam)
-
+    command = f"featureCounts -a {bin_file}"\
+              f"-F SAF"\
+              f"-o {count_file}"\
+              f"-O"\
+              f"{bam_file}"
+                
     rc = subprocess.call(command, shell=True)
 
     # Reshape the results, calculate the CPM and output the matrix
-    ###
+    mat_converter(sample_name, count_file, matrix_file)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--region', help='region info in bed format.')
@@ -97,3 +107,5 @@ parser.add_argument('--bam', help='bam file input.')
 parser.add_argument('--bin_num', help='Bin numbers')
 parser.add_argument('--bin_size', help='Bin size')
 args = parser.parse_args()
+
+plot_heatmap(args.sample_name, args.bam, args.bin_num, args.bin_size)
